1f3dc43152af7f6fbaea99abc5a159afd50730ad
Raghuram Elancheran <83778193+raghurame@users.noreply.github.com>
2022-07-31 22:30:15 +0530

    added input.config file to get user input

--- cylindricalMicelle.c
+++ cylindricalMicelle.c
@@ -19,10 +19,34 @@ Use bond information from the input files to create bond information for the ass
 IMPORTANT: Take all the input values from a config file
 */
 
+typedef struct surfactant
+{
+	int nMolecules;
+	float packingFactor;
+	char filename[100];
+} SURFACTANT;
+
+int checkNSurfactants (FILE *readConfig)
+{
+	
+}
+
 int main(int argc, char const *argv[])
 {
 	FILE *readConfig;
 	readConfig = fopen (argv[1], "r");
+
+	/*
+		These are the parameters to take from input config file:
+			
+			1. Total number of surfactant molecules
+			2. Input surfactant file name along with the total number of molecules
+			3. Packing factor of each molecules
+
+			NOTE: Any line starting with '#' can be considered as commented line
+	*/
+
+	int nSurfactants = checkNSurfactants (readConfig); // This value is coming from input config file.
 
 	fclose (readConfig);
 	return 0;
