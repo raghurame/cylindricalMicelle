4f1a5b1229185f0cbda1f1951dc6bd8b10fa3a7d
Raghuram <83778193+raghurame@users.noreply.github.com>
2022-08-11 17:33:23 +0530

    daily updates

--- cylindricalMicelle.c
+++ cylindricalMicelle.c
@@ -7,20 +7,7 @@
 #include "structs.h"
 #include "readInputs.h"
 #include "packing.h"
-
-/*
-Get the number of input surfactant molecules from user
-Read and save all the coordinates and bond information from all the surfactant PDB molecule
-Orient the surfactant molecule along X axis
-Using the packing factor for each surfactant, calculate the angle between the surfactants
-Using the angle between surfactant molecules in a single layer, find the total number of surfactant molecules in a layer
-Using the number of surfactant molecules in a layer, calculate the number of layers to add
-According to the packing angle, calculate the number of surfactant molecules to fit in one layer
-Replicate the layer along the axis of the cylinder
-Use bond information from the input files to create bond information for the assembled cylindrical micelle
-
-IMPORTANT: Take all the input values from a config file
-*/
+#include "printOutput.h"
 
 int main(int argc, char const *argv[])
 {
@@ -65,9 +52,9 @@ int main(int argc, char const *argv[])
 	BONDS *outputBonds;
 
 	outputCoordinates = replicateSurfactants (inputCoordinates, inputBonds, globalSurfactantlo, globalSurfactanthi, nSurfactants, inputStructures);
+	int totalAtoms = countTotalAtoms (inputStructures, nSurfactants);
 
 	// Testing results from replicateSurfactants function
-	// int totalAtoms = countTotalAtoms (inputStructures, nSurfactants);
 	// for (int i = 0; i < totalAtoms; ++i)
 	// {
 	// 	printf("%f %f %f %s %s %s\n", outputCoordinates[i].x, outputCoordinates[i].y, outputCoordinates[i].z, outputCoordinates[i].atomName1, outputCoordinates[i].atomName2, outputCoordinates[i].molName);
@@ -78,7 +65,6 @@ int main(int argc, char const *argv[])
 	outputBonds = addBonds (outputCoordinates, inputCoordinates, inputBonds, inputStructures, nSurfactants);
 
 	// Testing results from addBonds function
-	// int totalAtoms = countTotalAtoms (inputStructures, nSurfactants);
 	// for (int i = 0; i < totalAtoms; ++i)
 	// {
 	// 	printf("%3d %3d %3d %3d %3d %3d\n", outputBonds[i].atom1, outputBonds[i].atom2, outputBonds[i].atom3, outputBonds[i].atom4, outputBonds[i].atom5, outputBonds[i].atom6);
@@ -87,8 +73,8 @@ int main(int argc, char const *argv[])
 	// }
 
 	// Save the above information as *.car and *.mdf files
-	writeCar ();
-	writeMdf ();
+	writeCar (outputCoordinates, totalAtoms, inputStructures, nSurfactants);
+	writeMdf (outputBonds, totalAtoms, inputStructures, nSurfactants);
 
 	free (inputStructures);
 	fclose (readConfig);
