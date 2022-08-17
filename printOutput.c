4368ece029f22ac668efca24a9e3a6ee63fa6cd8
Raghuram <83778193+raghurame@users.noreply.github.com>
2022-08-17 16:55:30 +0530

    daily updates

--- printOutput.c
+++ printOutput.c
@@ -9,10 +9,52 @@
 #include "readInputs.h"
 #include "packing.h"
 
+void computeOutputBounds (CARTESIAN *lowerBounds, CARTESIAN *upperBounds, COORDINATES *outputCoordinates, int totalAtoms)
+{
+	for (int i = 0; i < totalAtoms; ++i)
+	{
+		if (i == 0) {
+			(*lowerBounds).x = outputCoordinates[i].x; (*lowerBounds).y = outputCoordinates[i].y; (*lowerBounds).z = outputCoordinates[i].z; (*upperBounds).x = outputCoordinates[i].x; (*upperBounds).y = outputCoordinates[i].y; (*upperBounds).z = outputCoordinates[i].z; }
+		else
+		{
+			if (outputCoordinates[i].x < (*lowerBounds).x)
+			{
+				(*lowerBounds).x = outputCoordinates[i].x;
+			}
+			if (outputCoordinates[i].x > (*upperBounds).x)
+			{
+				(*upperBounds).x = outputCoordinates[i].x;
+			}
+			if (outputCoordinates[i].y < (*lowerBounds).y)
+			{
+				(*lowerBounds).y = outputCoordinates[i].y;
+			}
+			if (outputCoordinates[i].y > (*upperBounds).y)
+			{
+				(*upperBounds).y = outputCoordinates
+			}
+			if (outputCoordinates[i].z < (*lowerBounds).z)
+			{
+				/* code */
+			}
+			if (outputCoordinates[i].z > (*upperBounds).z)
+			{
+				/* code */
+			}
+		}
+	}
+}
+
 void writeCar (COORDINATES *outputCoordinates, int totalAtoms, SURFACTANT *inputStructures, int nSurfactants)
 {
 	FILE *outputCAR;
 	outputCAR = fopen ("finalStructure.car", "w");
+
+	CARTESIAN lowerBounds, upperBounds;
+
+	computeOutputBounds (&lowerBounds, &upperBounds, outputCoordinates, totalAtoms);
+
+	fprintf(outputCAR, "!BIOSYM archive 3\nPBC=ON\nMaterials Studio Generated CAR File\n!DATE Mon Nov 26 21:42:01 2012\nPBC   50.0000   50.0000   53.9263   90.0000   90.0000   90.0000 (P1)\n");
 
 	for (int i = 0; i < totalAtoms; ++i) {
 		fprintf(outputCAR, "%s%-5d%14.9f%14.9f%14.9f XXXX AF1%6s*%7s%8.3f\n", outputCoordinates[i].atomName1, i, outputCoordinates[i].x, outputCoordinates[i].y, outputCoordinates[i].z, outputCoordinates[i].atomName1, outputCoordinates[i].atomName1, outputCoordinates[i].col10); }
@@ -25,6 +67,8 @@ void writeMdf (COORDINATES *outputCoordinates, BONDS *outputBonds, int totalAtom
 	FILE *outputMDF;
 	outputMDF = fopen ("finalStructure.mdf", "w");
 
+	fprintf(outputMDF, "!BIOSYM molecular_data 4\n\n!Date: Mon Nov 26 21:42:02 2012   Materials Studio Generated MDF file\n\n#topology\n\n@column 1 element\n@column 2 atom_type\n@column 3 charge_group\n@column 4 isotope\n@column 5 formal_charge\n@column 6 charge\n@column 7 switching_atom\n@column 8 oop_flag\n@column 9 chirality_flag\n@column 10 occupancy\n@column 11 xray_temp_factor\n@column 12 connections\n\n@molecule MODEL6\n\n");
+
 	int atomID1, atomID2, atomID3, atomID4, atomID5;
 
 	for (int i = 0; i < totalAtoms; ++i)
@@ -35,8 +79,8 @@ void writeMdf (COORDINATES *outputCoordinates, BONDS *outputBonds, int totalAtom
 		atomID4 = outputBonds[i].atom4;
 		atomID5 = outputBonds[i].atom5;
 
-		printf("%d %d %d %d\n", outputBonds[i].atom1, outputBonds[i].atom2, outputBonds[i].atom3, outputBonds[i].atom4, outputBonds[i].atom5);
-		usleep (100000);
+		// printf("%d %d %d %d\n", outputBonds[i].atom1, outputBonds[i].atom2, outputBonds[i].atom3, outputBonds[i].atom4, outputBonds[i].atom5);
+		// usleep (100000);
 
 		fprintf(outputMDF, "XXXX_AF1:%s%-5d%5s%4s*%7s%6d%3d%11.4f%2d%2d%2d%7.4f%8.4f", outputCoordinates[i].atomName1, i, outputCoordinates[i].atomName1, outputCoordinates[i].atomName1, "?", 0, 0, outputCoordinates[i].col10, 0, 0, 8, 1.0, 0.0);
 
